beae13ac606643232130f383a82c831f10fb16b1
Anton Mashko <anton.mashko@gmail.com>
2019-06-02 00:01:50 +0300

    improve phone parser

--- phonenumber.go
+++ phonenumber.go
@@ -9,65 +9,72 @@ import (
 var ErrUnsupportableFormat = errors.New("phone number format is unsupportable")
 var ErrInvalidCharactersInPhoneNumber = errors.New("invalid character")
 
-var zeroNumber = PhoneNumber{}
+type PhoneNumber uint64
 
-type PhoneNumber struct {
-	CountyCode   uint16
-	OperatorCode uint16
-	Local        [4]byte
+func (p PhoneNumber) CountyCode() byte {
+	return byte(p >> 48)
+}
+
+func (p PhoneNumber) Operator() uint16 {
+	return uint16(p >> 32)
+}
+
+func (p PhoneNumber) Local() uint32 {
+	return uint32(p)
 }
 
 func (p PhoneNumber) String() string {
 	var result bytes.Buffer
 	result.WriteByte('+')
-	result.WriteString(strconv.Itoa(int(p.CountyCode)))
-	result.WriteString(strconv.Itoa(int(p.OperatorCode)))
-	for _, l := range p.Local {
-		if l < 10 {
-			result.WriteByte('0')
-		}
-		result.WriteByte(l)
+	result.WriteString(strconv.Itoa(int(p.CountyCode())))
+	if op := p.Operator(); op < 100 {
+		result.WriteByte('0')
+		result.WriteString(strconv.Itoa(int(op)))
+	} else {
+		result.WriteString(strconv.Itoa(int(op)))
+	}
+	if l := p.Local(); l < 1000000 {
+		result.WriteString(strconv.Itoa(int(l)))
+	} else {
+		result.WriteString(strconv.Itoa(int(l)))
 	}
 	return result.String()
 }
 
 func Parse(number string) (PhoneNumber, error) {
 	buffer := bytes.NewBuffer([]byte{})
+	// remove all invalid chars and delimeters from input
 	for i := 0; i < len(number); i++ {
 		if number[i] >= '0' && number[i] <= '9' {
 			buffer.WriteByte(number[i])
 		}
 	}
 	number = buffer.String()
-	var result PhoneNumber
+	var result uint64
 	switch len(number) {
 	case 11, 12:
-		code, err := strconv.ParseUint(number[0:len(number)-10], 10, 16)
-		if err != nil {
-			return zeroNumber, err
+		// parse county code
+		if len(number) == 12 {
+			result |= uint64((number[0]-'0')*10+number[1]-'0') << 48
+		} else if len(number) == 11 {
+			result |= uint64(number[0]-'0') << 48
 		}
-		result.CountyCode = uint16(code)
 		fallthrough
 	case 10:
 		// parse domestic operator
-		code, err := strconv.ParseUint(number[len(number)-10:len(number)-7], 10, 16)
-		if err != nil {
-			return zeroNumber, err
-		}
-		result.OperatorCode = uint16(code)
+		result |= uint64((uint16(number[len(number)-10]-'0')*100)+(uint16(number[len(number)-9]-'0')*10)+(uint16(number[len(number)-8]-'0'))) << 32
 		fallthrough
 	case 7:
 		// parse local part
-		n := number[len(number)-7 : len(number)]
-		index := uint8(3)
-		const zeroByte = '0'
-		for i := len(n) - 1; i > 1; i -= 2 {
-			result.Local[index] = (n[i-1]-zeroByte)*10 + n[i] - zeroByte
-			index--
+		var loc uint32
+		pos := uint32(1000000)
+		for i := len(number) - 7; i < len(number); i++ {
+			loc += uint32(number[i]-'0') * pos
+			pos /= 10
 		}
-		result.Local[index] = n[0] - zeroByte
-		return result, nil
+		result |= uint64(loc)
+		return PhoneNumber(result), nil
 	default:
-		return zeroNumber, ErrUnsupportableFormat
+		return 0, ErrUnsupportableFormat
 	}
 }
--- phonenumber_test.go
+++ phonenumber_test.go
@@ -1,7 +1,6 @@
 package phonenumber
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -10,37 +9,16 @@ func TestPhoneNumberString(t *testing.T) {
 	if err != nil {
 		t.Error("failed to parse.", err)
 	}
+	t.Logf("result %s", ph.String())
 	if ph.String() != "+15417540301" {
-		t.Fail()
+		t.Error("Phone doesn't equal.")
 	}
+	t.Log(ph.Local())
 }
 
-func TestParse(t *testing.T) {
-	type args struct {
-		number string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    PhoneNumber
-		wantErr bool
-	}{
-		{name: "Local", args: args{"754-3010"}, want: PhoneNumber{Local: [4]byte{7, 54, 30, 10}}},
-		{name: "Domestic", args: args{"(541) 754-3010"}, want: PhoneNumber{OperatorCode: 541, Local: [4]byte{7, 54, 30, 10}}},
-		{name: "International", args: args{"+1-541-754-3010"}, want: PhoneNumber{CountyCode: 1, OperatorCode: 541, Local: [4]byte{7, 54, 30, 10}}},
-		{name: "Dialed in the US", args: args{"1-541-754-3010"}, want: PhoneNumber{CountyCode: 1, OperatorCode: 541, Local: [4]byte{7, 54, 30, 10}}},
-		{name: "Invalid input", args: args{"fooNumber"}, wantErr: true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := Parse(tt.args.number)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Parse() = %v, want %v", got, tt.want)
-			}
-		})
+func TestUnsupportableFormat(t *testing.T) {
+	_, err := Parse("+12345678901234")
+	if err == nil {
+
 	}
 }
